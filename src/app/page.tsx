c9727515d012b2be5fd35b8c6f2c471057e01b90
Batuhan445 <neomatrix44@hotmail.com>
2023-08-18 16:48:53 +0300

    some css fixed and import changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,31 @@ import React, { useEffect, useState } from "react";
 import NewTodoInput from "@/components/NewTodoInput";
 import NewTodoAddButton from "@/components/NewTodoAddButton";
 import ClearCompleteButton from "@/components/ClearCompleteButton";
-import { loadListFromLocalStorage, saveListToLocalStorage } from "@/components/LocalStroage";
+import {loadListFromLocalStorage, saveListToLocalStorage} from "@/components/LocalStroage";
+import NewTodoList from "@/components/NewTodoList";
 import moment from "moment";
 import "moment/locale/tr";
 import TodoDate from "@/components/TodoDate";
+import Image from "next/image";
+import { FaEdit, FaTimes } from "react-icons/fa";
+import { LuSave } from "react-icons/lu";
 
 interface todoListesi {
   id: number;
   başlık: any;
   isDone: boolean;
   oluşturmaTarihi: string;
-  düzenlemeTarihi: string
+  düzenlemeTarihi: string;
 }
 
 export default function Page() {
   const [liste, setListe] = useState<todoListesi[]>([]);
   const [yeniBaşlık, setYeniBaşlık] = useState<string>("");
   const [editingItemIds, setEditingItemIds] = useState<number[]>([]);
-  const [editedTitles, setEditedTitles] = useState<{ [key: number]: string }>({});
-  
+  const [editedTitles, setEditedTitles] = useState<{ [key: number]: string }>(
+    {}
+  );
+
   
 
   // Save to Local Stroage
@@ -47,7 +53,7 @@ export default function Page() {
         başlık: yeniBaşlık,
         isDone: false,
         oluşturmaTarihi: moment().format("DD-MM-YYYY HH:mm:ss"),
-        düzenlemeTarihi: ""
+        düzenlemeTarihi: "",
       };
       setListe([...liste, newTodo]);
       setYeniBaşlık("");
@@ -56,16 +62,14 @@ export default function Page() {
 
   // New todo add is done
 
-  const tamamlandıİşlemi = (id: number) => {
+  const newTodoAppIsDone = (id: number) => {
     setListe(
       liste.map((eleman) =>
-        eleman.id === id
-          ? { ...eleman, isDone: !eleman.isDone }
-          : eleman
+        eleman.id === id ? { ...eleman, isDone: !eleman.isDone } : eleman
       )
     );
   };
-  
+
   // Start Edit Todo
 
   const startEditing = (id: number, title: string) => {
@@ -78,14 +82,20 @@ export default function Page() {
   const saveEditedTitle = (id: number) => {
     setListe((prevListe) =>
       prevListe.map((item) =>
-        item.id === id ? { ...item, başlık: editedTitles[id], düzenlemeTarihi: moment().format(("DD-MM-YYYY HH:mm:ss")) } : item
+        item.id === id
+          ? {
+              ...item,
+              başlık: editedTitles[id],
+              düzenlemeTarihi: moment().format("DD-MM-YYYY HH:mm:ss"),
+            }
+          : item
       )
     );
     cancelEditing(id);
   };
-  
+
   // Todo Edit Cancel
-  
+
   const cancelEditing = (id: number) => {
     setEditingItemIds((prevIds) => prevIds.filter((prevId) => prevId !== id));
     setEditedTitles((prevTitles) => {
@@ -114,109 +124,107 @@ export default function Page() {
   return (
     <div className="mt-10">
 
-      <h1 className="text-center text-3xl">Todo App</h1>
-
-      <div className="flex justify-center mt-4">
-        <NewTodoInput
-          value={yeniBaşlık}
-          onChange={setYeniBaşlık}
-          onEnterKey={yeniTodoEkle}
+      <div className="fixed inset-0">
+        <Image
+          src="/img/background.jpg"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          className="z-0"
         />
-
-        <NewTodoAddButton onTodoAddButton={() => yeniTodoEkle()} />
-
       </div>
 
-      <div className=" mt-2 text-center">
-        Eğer bir listeyi tamamladıysanız o listenin üstüne tıklayarak "Tamamlananları Temizle" butonuna tıklayabilirsiniz
-      </div>
+      <div className="relative">
 
-      <div className="mt-4">
-        <div className="overflow-y-auto h-[30rem] md:h-96 w-4/5 m-auto border-4 rounded-xl">
-          {liste.map((item) => (
+        <h1 className="text-center text-3xl text-white">Todo App</h1>
 
-            <div className="grid md:flex md:py-2 md:flex-row gap-4 items-center border-b-4 border-gray-500">
+        <div className="flex justify-center mt-4">
+          <NewTodoInput
+            value={yeniBaşlık}
+            onChange={setYeniBaşlık}
+            onEnterKey={yeniTodoEkle}
+          />
 
-              <div className="text-center mt-2 md:mt-0 md:ml-4 md:w-8">#{item.id}</div>
-
-              {editingItemIds.includes(item.id) ? (
-
-                <div className="flex items-center m-auto w-2/3">
-
-                  <input
-                    className="border m-auto px-2 py-1 rounded bg-blue-400 outline-blue-500 text-white placeholder:text-white placeholder:text-left text-center w-1/2"
-                    value={editedTitles[item.id] || ""}
-                    placeholder="Todo Düzenle..."
-                    onChange={(e) =>
-                      setEditedTitles((previousTitle) => ({
-                        ...previousTitle,
-                        [item.id]: e.target.value,
-                      }))
-                    }
-                  />
-
-                  <div className="space-x-3">
-
-                    <button
-                      className="border bg-green-500 text-white px-2 py-1 rounded"
-                      onClick={() => saveEditedTitle(item.id)}
-                    >
-                      Kaydet
-                    </button>
-
-                    <button
-                      className="border bg-red-500 text-white px-2 py-1 rounded"
-                      onClick={() => cancelEditing(item.id)}
-                    >
-                      İptal
-                    </button>
-
-                  </div>
-
-                </div>
-
-              ) : (
-
-                <div className="grid grid-flow-row items-center m-auto md:flex w-2/3">
-
-                  <div
-                    className={`cursor-pointer border m-auto px-2 py-1 rounded w-1/2 text-center bg-blue-500 text-white ${
-                      item.isDone ? "bg-red-500" : ""
-                    }`}
-                    onClick={() => tamamlandıİşlemi(item.id)}
-                  >
-                    {item.başlık}
-                  </div>
-
-                  <button
-                    className="border md:ml-auto bg-blue-500 text-white px-2 py-1 rounded"
-                    onClick={() => startEditing(item.id, item.başlık)}
-                  >
-                    Düzenle
-                  </button>
-
-                </div>
-
-              )}
-
-              <TodoDate 
-              creationDate={item.oluşturmaTarihi} 
-              editedDate={item.düzenlemeTarihi}
-              />
-
-            </div>
-
-          ))}
-
+          <NewTodoAddButton onTodoAddButton={() => yeniTodoEkle()} />
         </div>
 
-      </div>
+        <div className="mt-4">
 
-      <div className="flex justify-center mt-4">
+          <div className=" w-4/5 m-auto rounded-xl">
+
+            {liste.map((item) => (
+              <div className="grid md:flex md:py-2 md:flex-row gap-4 items-center border-b-4 border-gray-500">
+
+                <div className="text-center text-white mt-2 md:mt-0 md:ml-4 md:w-8">
+                  #{item.id}
+                </div>
+
+                {editingItemIds.includes(item.id) ? (
+                  <div className="flex justify-between items-center m-auto w-2/3 md:w-3/5">
+
+                    <input
+                      className="border px-2 py-3 rounded bg-blue-400 outline-none border-blue-400 text-white placeholder:text-white  w-2/3"
+                      value={editedTitles[item.id] || ""}
+                      placeholder="Todo Düzenle..."
+                      onChange={(e) =>
+                        setEditedTitles((previousTitle) => ({
+                          ...previousTitle,
+                          [item.id]: e.target.value,
+                        }))
+                      }
+                    />
+
+                    <div className="space-x-3">
+
+                      <button
+                        className="border bg-green-500 border-green-500 text-white px-4 py-3 rounded"
+                        onClick={() => saveEditedTitle(item.id)}
+                        style={{ fontSize: "20px" }}
+                      >
+                        <LuSave />
+                      </button>
+
+                      <button
+                        className="border bg-red-500 border-red-500 text-white px-4 py-3 rounded"
+                        onClick={() => cancelEditing(item.id)}
+                        style={{ fontSize: "20px" }}
+                      >
+                        <FaTimes />
+                      </button>
+
+                    </div>
+
+                  </div>
+                ) : (
+
+                  <NewTodoList
+                    key={item.id}
+                    item={item}
+                    newTodoAdded={newTodoAppIsDone}
+                    startEditing={startEditing}
+                  />
+
+                )}
+
+                <div className="m-auto md:mr-auto">
+                  <TodoDate
+                    creationDate={item.oluşturmaTarihi}
+                    editedDate={item.düzenlemeTarihi}
+                  />
+                </div>
+
+              </div>
+
+            ))}
+          </div>
+        </div>
+
+      <div className="flex justify-center mt-4 mb-4">
         {clearButton && <ClearCompleteButton onClearCompleted={clearIsDone} />}
       </div>
+
+      </div>
+
     </div>
   );
 }
-
-
