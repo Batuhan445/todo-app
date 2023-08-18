683fc53cae85809f986ea7503b0d6a10a39b5547
Batuhan445 <neomatrix44@hotmail.com>
2023-08-18 16:45:32 +0300

    imported new todo list

---
+++ src/components/NewTodoList.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaEdit } from "react-icons/fa";
+
+interface TodoItem {
+  id: number;
+  başlık: any;
+  isDone: boolean;
+}
+
+interface NewTodoListProps {
+  item: TodoItem;
+  newTodoAdded: (id: number) => void;
+  startEditing: (id: number, başlık: string) => void;
+}
+
+const newTodoList: React.FunctionComponent<NewTodoListProps> = ({
+  item,
+  newTodoAdded,
+  startEditing,
+}) => {
+
+  return (
+
+    <div className="flex justify-between items-center m-auto  w-3/5">
+
+      <div
+        className={`cursor-pointer border py-3 rounded w-2/3 pl-4 bg-blue-500 border-blue-500 text-white ${
+          item.isDone ? "line-through" : ""
+        }`}
+        onClick={() => newTodoAdded(item.id)}
+      >
+
+        {item.başlık}
+
+      </div>
+
+      <button
+        className="border md:mt-0 border-blue-500 bg-blue-500 text-white px-4 py-3 rounded"
+        onClick={() => startEditing(item.id, item.başlık)}
+        style={{ fontSize: "20px" }}
+      >
+
+        <FaEdit />
+
+      </button>
+
+    </div>
+  );
+};
+
+export default newTodoList;
